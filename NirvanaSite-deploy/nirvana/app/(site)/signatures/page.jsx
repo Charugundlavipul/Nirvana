ac1b482f5b4e19d41f97f0cf537bef75be1a6359
Charugundlavipul <charugundlavipul@gmail.com>
2026-03-30 23:17:28 -0400

    feat: implement email signature preview component and page route

---
+++ NirvanaSite-deploy/nirvana/app/(site)/signatures/page.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'Signatures | Nirvana',
+  description: 'Signature previews for Nirvana Luxe',
+};
+
+import SignaturePreview from '../../../src/signatures/SignaturePreview';
+
+export default function SignaturesPage() {
+  return <SignaturePreview />;
+}
