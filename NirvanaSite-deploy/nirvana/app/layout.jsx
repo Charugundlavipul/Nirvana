23dfde388f9cc9e32f1dcc5b319bcd6e1d176fd3
Charugundlavipul <charugundlavipul@gmail.com>
2026-08-28 11:31:11 -0400

    feat: initialize RootLayout with SEO metadata, custom fonts, and third-party tracking scripts

--- NirvanaSite-deploy/nirvana/app/layout.jsx
+++ NirvanaSite-deploy/nirvana/app/layout.jsx
@@ -70,6 +70,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${firaSans.variable} ${firaCondensed.variable}`}>
       <head>
         <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="preconnect" href="https://connect.facebook.net" />
       </head>
       <body>
         <a href="#main-content" className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[100000] focus:bg-white focus:text-black focus:px-4 focus:py-2 focus:rounded focus:shadow-lg focus:text-sm focus:font-semibold">Skip to main content</a>
@@ -86,6 +87,29 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-XE6P41ERZ5');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1720097852648123');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1720097852648123&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
