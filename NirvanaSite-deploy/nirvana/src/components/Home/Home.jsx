c8b859cd147f3c678fb3d5a8da35228d0999ea21
Charugundlavipul <charugundlavipul@gmail.com>
2026-09-16 19:39:21 -0400

    feat: add Home page component with property listings and interactive carousels

--- NirvanaSite-deploy/nirvana/src/components/Home/Home.jsx
+++ NirvanaSite-deploy/nirvana/src/components/Home/Home.jsx
@@ -27,6 +27,15 @@ const getPropertyCardImages = (property) => {
     .slice(0, 5);
 };
 
+const shuffleArray = (array) => {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+};
+
 const Home = ({ initialProperties = [], initialReviews = [] }) => {
   const router = useRouter();
 
@@ -46,7 +55,7 @@ const Home = ({ initialProperties = [], initialReviews = [] }) => {
   const BADGES = ["Most Popular", "Featured", "New"];
 
   useEffect(() => {
-    const homeProperties = initialProperties || [];
+    const homeProperties = shuffleArray(initialProperties || []);
     setProperties(homeProperties);
 
     const indices = {};
